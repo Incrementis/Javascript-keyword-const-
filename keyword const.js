1aa7a3f53f1ca419af2baf41473e56ec4e823d96
Akin C <no-budget-games@outlook.de>
2017-01-17 20:26:43 +0100

    Javascript keyword "const"
    Checking the usability of the keyword "const".

---
+++ keyword const.js
@@ -0,0 +1,8 @@
+//the famous number 42
+const constant = 42;
+
+//Trying to change the variables content
+constant = "constant was modified";
+
+//shows the value of the variable
+alert("value: " + constant);
